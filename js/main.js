a0153219f9dd65947f81635e19c6dc294e02f43a
Charlene Yee <150361629+charyee@users.noreply.github.com>
2024-07-31 00:48:29 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,82 @@
+window.onload = () => {
+    const transition_el = document.querySelector('.transition');
+    setTimeout(() => {
+        transition_el.classList.remove('is-active');
+    }, 500);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const transitionElement = document.querySelector('.transition');
+
+    setTimeout(() => {
+        transitionElement.classList.remove('is-active');
+    }, 500); // Match your transition duration
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'ArrowRight') {
+            goToNextPage();
+        } else if (event.key === 'ArrowLeft') {
+            goToPreviousPage();
+        }
+    });
+
+    function goToNextPage() {
+        const currentPage = window.location.pathname.split('/').pop();
+        let nextPage = '';
+
+        switch (currentPage) {
+            case 'index.html':
+            case '':
+                nextPage = 'html/experience.html';
+                break;
+            case 'experience.html':
+                nextPage = 'html/projects.html';
+                break;
+            case 'projects.html':
+                nextPage = 'html/contact.html';
+                break;
+            case 'contact.html':
+                nextPage = 'index.html';
+                break;
+            default:
+                nextPage = 'index.html';
+                break;
+        }
+
+        applyTransitionAndNavigate(nextPage);
+    }
+
+    function goToPreviousPage() {
+        const currentPage = window.location.pathname.split('/').pop();
+        let prevPage = '';
+
+        switch (currentPage) {
+            case 'index.html':
+            case '':
+                prevPage = 'html/contact.html';
+                break;
+            case 'experience.html':
+                prevPage = 'index.html';
+                break;
+            case 'projects.html':
+                prevPage = 'html/experience.html';
+                break;
+            case 'contact.html':
+                prevPage = 'html/projects.html';
+                break;
+            default:
+                prevPage = 'index.html';
+                break;
+        }
+
+        applyTransitionAndNavigate(prevPage);
+    }
+
+    function applyTransitionAndNavigate(page) {
+        transitionElement.classList.add('is-active');
+        
+        setTimeout(() => {
+            window.location.href = page;
+        }, 500); // Match this with your CSS transition duration
+    }
+});
